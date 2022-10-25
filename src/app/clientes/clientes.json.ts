cd20a97ab89ed3ac03ce6c3a0f1d710bd1d45be6
Martin Diaz <cavallinorampante@gmail.com>
2022-10-24 23:54:19 -0300

    primer commit

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,8 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Martín', apellido: 'Díaz', email: 'martinari_77@hotmail.com', createAt:'2017-01-27' },
+    {id: 2, nombre: 'Bafomet', apellido: 'Magnussen', email: 'bafotumachofacho@hotmail.com', createAt:'2016-04-22' },
+    {id: 1, nombre: 'Salvatore', apellido: 'Valentino', email: 'salva@hotmail.com', createAt:'2017-01-27' },
+    {id: 1, nombre: 'Alejandro', apellido: 'Valentino', email: 'pela@hotmail.com', createAt:'2017-01-27' },
+  ];
